a28358862419517157a46b4252836bfb04e78dd5
RetargetingCL <54725593+RetargetingCL@users.noreply.github.com>
2019-12-05 10:33:10 -0300

    Módulos de Pasarela de Pagos
    -Webpay
    -Khipu
    -Flow
    -Mercadopago

---
+++ payment_mercadopago/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'MercadoPago Payment Acquirer',
+    'category': 'Accounting',
+    'summary': 'Payment Acquirer: MercadoPago Implementation for Odoo version 12.0',
+    'version': '12.0',
+    'description': """MercadoPago Payment Acquirer""",
+    'author': 'Moldeo Interactive',
+    'website': 'http://www.moldeointeractive.com.ar',
+    'depends': ['payment'],
+    'data': [
+        'views/mercadopago.xml',
+        'views/payment_acquirer.xml',
+        #'views/res_config_view.xml',
+        'data/mercadopago.xml',
+    ],
+    'installable': True,
+    'post_init_hook': 'create_missing_journal_for_acquirers',
+}
